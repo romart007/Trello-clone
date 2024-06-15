9086101e9ac78e1a715cd53ee54b9211c773f536
romart007 <labb0ssfreelance@gmail.com>
2024-06-15 16:06:52 +0800

    clean all files

--- src/data/links.js
+++
@@ -1,7 +0,0 @@
-export const drawerLinks = [
-  { icon: "home", text: "Home" },
-  { icon: "music_video", text: "Shorts" },
-  { icon: "ondemand_video", text: "Subscribe" },
-  { icon: "video_library", text: "Library" },
-  { icon: "restore", text: "History" },
-];
--- src/layouts/MainLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <router-view />
-  </q-layout>
-</template>
-
-<script setup>
-defineOptions({ name: "MainLayout" });
-</script>
